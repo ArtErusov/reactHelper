1a0eec2df555dc1f529703e08e1e18d5207516db
Artem Erusov <erusovav@gmail.com>
2025-03-01 16:31:23 +0300

    Created a new element SmallJsApps

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import SmallJsApps from "./pages/smallJsApps/SmallJsApps";
 import UiElement from "./pages/UiElement/UiElement";
 import styles from './styles.module.css';
 function App() {
@@ -5,6 +6,7 @@ function App() {
 return (
   <div className={styles.container}>
     <UiElement />
+    <SmallJsApps />
   </div>
   )
 }
---
+++ src/pages/smallJsApps/SmallJsApps.jsx
@@ -0,0 +1,16 @@
+import styles from './styles.module.css';
+
+const SmallJsApps = () => {
+  return (
+    <>
+      <div className={styles.temporaryStub}>smallJsApps</div>
+      {/* удалить после роутинга */}
+      <div className={styles.grid__one}>
+        <div className={styles.grid__one__one}>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SmallJsApps;
